baea0e08363af2f1020ed45cb9f05503681a2355
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-23 18:21:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 
-def speak_to_grandma(str)
-  if str == "I LOVE YOU GRANDMA!"
-   "I LOVE YOU TOO PUMPKIN!"
-  unless str == str.upcase
+def speak_to_grandma(phrase)
+  if phrase == 'I LOVE YOU GRANDMA!'
+    'I LOVE YOU TOO PUMPKIN!'
+  elsif phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
-  else 
+  else
     "NO, NOT SINCE 1938!"
   end
 end
