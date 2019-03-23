5f8a4f59ee9aa3abc3e8f1815d91375b6bd00d0b
Endy Ranaudo <endyranaudo@gmail.com>
2019-03-23 18:19:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(str)
   if str == "I LOVE YOU GRANDMA!"
    "I LOVE YOU TOO PUMPKIN!"
-  elsif str != str.upcase
+  unless str == str.upcase
     "HUH?! SPEAK UP, SONNY!"
   else 
     "NO, NOT SINCE 1938!"
